96096db41d8120af8ce6b9def7d9b31a9d61e983
omar <omaraldyb2020@gmail.com>
2025-09-20 13:29:44 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import viteSvgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSvgr()],
+  server: {
+    historyApiFallback: true,
+  },
 })
